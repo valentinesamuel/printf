6d82466059da5a0372ff2391d79eda3ef79c44ad
nimikadejo <kadejotoluwanimi@gmail.com>
2022-07-11 15:52:53 -0700

    function to print string rot13

---
+++ print_rot13.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_rot13 - prints a string in rot13
+ * @R: string to print
+ * Return: number of chars printed
+ */
+int print_rot13(va_list R)
+{
+	char *str;
+	unsigned int i, j;
+	int count = 0;
+	char in[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char out[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	str = va_arg(R, char *);
+	if (str == NULL)
+		str = "(ahyy)";
