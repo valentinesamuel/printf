080a6bff031f42f744489e257e16e43a6575f5a7
valentinesamuel <valentinesamuel2580@gmail.com>
2022-07-12 07:32:02 -0700

    added char_validator

---
+++ char_validators.c
@@ -0,0 +1,30 @@
+/*#include "main.h"*/
+#define NULL 0
+
+/**
+ * is_printable - checks if a character is printable
+ * @c: character to check
+ *
+ * Return: 1 if printable, 0 otherwise
+ */
+int is_printable(char c)
+{
+	if (c >= 32 && c < 127)
+	{
+		return (1);
+	}
+	return (0);
+}
+
+/**
+ * _isalpha - checks if character is alpha or not
+ * @c: character to check
+ *
+ * Return: 1 if is alpha, 0 otherwise
+ */
+int _isalpha(char c)
+{
+	if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
+		return (1);
+	return (0);
+}
