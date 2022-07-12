c2cdc192f2d48f2c5f516149e59130815bbcc2fb
nimikadejo <kadejotoluwanimi@gmail.com>
2022-07-12 08:12:03 -0700

    remove comment

--- _putchar.c
+++ _putchar.c
@@ -2,7 +2,7 @@
 
 /**
  * _putchar - writes the character c to stdout
- * @c: The character to print
+ * @c: the character to print
  * Return: On success 1
  * Otherwise, -1 is returned, and errno is set appropriately
  */
