2151a9edc0a1260ad6ae3b9a5bff9265c6ca6e47
valentinesamuel <valentinesamuel2580@gmail.com>
2022-07-12 07:12:11 -0700

    added main.h

--- main.h
+++ main.h
@@ -20,4 +20,27 @@ int print_bin(va_list b);
 int print_unsig(va_list u);
 int print_octal(va_list o);
 
+int print_x(va_list x);
+int print_X(va_list X);
+int print_rot13(va_list R);
+
+int _strlen(char *s);
+void _string_upper(char *str);
+char *_strcpy(char *dest, char *src);
+int is_printable(char c);
+int _isalpha(char c);
+int print_unsignedIntToHex(unsigned int num, char _case);
+int print_hex_base(va_list arg, char _case);
+
+/**
+ * struct c_format - code format foe each function
+ * @specifier: specifier after % sign
+ * @fp: function pointer to appropriate print function
+ **/
+typedef struct c_format
+{
+	char *specifier;
+	int (*fp)(va_list);
+} code_format;
+
 #endif
