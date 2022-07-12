7aa3f2cac66fe10e6921c90c2ce0ac220cceff47
valentinesamuel <valentinesamuel2580@gmail.com>
2022-07-12 07:19:00 -0700

    added unsigned printer

--- print_unsigned_int.c
+++ print_unsigned_int.c
@@ -7,8 +7,33 @@
  */
 int print_unsig(va_list u)
 {
-	int divisor = 1, i, resp;
-	unsigned int n = va_arg(u, unsigned int);
+	int divisor = 1, t, d, index = 0, highestorderdigit = 0, totalchars = 0;
+	unsigned int num;
+	char digits[12] = { 0 };
 
-	for (i = 0; n / divisor > 9; i++, divisor *= 10)
-
+	digits[0] = digits[0];
+	num = va_arg(u, int);
+	divisor = divisor * 1000000000;
+	for (t = 0; t <= 9; t++)
+	{
+		d = (num / divisor) % 10;
+		if ((d != 0) && !highestorderdigit)
+			highestorderdigit = 1;
+		if (highestorderdigit)
+		{
+			digits[index++] = d + '0';
+			_putchar(d + '0');
+			totalchars++;
+		}
+		divisor = divisor / 10;
+	}
+	if (totalchars == 0)
+	{
+		digits[index++] = '0';
+		digits[index] = '\0';
+		_putchar(d + '0');
+		totalchars++;
+	}
+	digits[index] = '\0';
+	return (totalchars);
+}
