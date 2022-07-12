d902a73de25a1526962395d86a139ab046832e08
valentinesamuel <valentinesamuel2580@gmail.com>
2022-07-12 07:11:34 -0700

    added get_format_func

--- get_format_funct.c
+++ get_format_funct.c
@@ -11,8 +11,24 @@ int (*get_format_function(char format))(va_list)
 	code_format get_func[] = {
 	{"c", print_char},
 	{"s", print_string},
+	{"S", print_upper_string},
 	{"i", print_int},
 	{"d", print_dec},
 	{"r", print_rev},
 	{"b", print_bin},
+	{"u", print_unsig},
+	{"o", print_octal},
+	{"x", print_x},
+	{"X", print_X},
+	{"R", print_rot13},
+	{"%", print_percent},
+	{NULL, NULL}
+	};
+	while (get_func[i].specifier != NULL)
+	{
+		if (get_func[i].specifier[0] == format)
+			return (get_func[i].fp);
+		i++;
 	}
+	return (NULL);
+}
