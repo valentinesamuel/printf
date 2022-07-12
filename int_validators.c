32d2e036c58d749a04554abdd6df81b47cbcf573
valentinesamuel <valentinesamuel2580@gmail.com>
2022-07-12 07:32:21 -0700

    added int_validator

---
+++ int_validators.c
@@ -0,0 +1,52 @@
+/*#include "main.h"*/
+
+/**
+ * print_unsignedIntToHex - prints unsigned int to hexadecimal.
+ * @num: number to print
+ * @_case: letter `a` on the case to print it (upper or lower)
+ * Return: number or char printed
+ */
+int print_unsignedIntToHex(unsigned int num, char _case)
+{
+	unsigned int num2;
+	int i, j, remainder, nbrCharacters = 0;
+	char *numhex;
+
+	for (num2 = num; num2 != 0; nbrCharacters++, num2 /= 16)
+	;
+
+	numhex = malloc(nbrCharacters);
+	for (i = 0; num != 0; i++)
+	{
+		remainder = num % 16;
+		if (remainder < 10)
+			numhex[i] = remainder + '0';
+		else
+			numhex[i] = remainder - 10 + _case;
+		num = num / 16;
+	}
+	for (j = i - 1; j >= 0; j--)
+		_putchar(numhex[j]);
+	free(numhex);
+	return (nbrCharacters);
+}
+
+
+/**
+ * print_hex_base - base function for printing hexadecimal numbers
+ * @arg: argument list containing hexadecimal
+ * @_case: a  in caps on lower, printing the hex number in caps in lower
+ * Return: number of digits printed
+ */
+
+int print_hex_base(va_list arg, char _case)
+{
+	unsigned int num = va_arg(arg, unsigned int);
+	int nbrCharacters;
+
+	if (num == 0)
+		return (_putchar('0'));
+	nbrCharacters = print_unsignedIntToHex(num, _case);
+
+	return (nbrCharacters);
+}
